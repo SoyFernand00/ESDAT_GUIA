3cbd4cf5591a4d90084c14af2386452855805853
SoyFernand00 <n00443358@upn.pe>
2026-05-12 13:32:44 -0500

    subir2

--- Arboles/Arbolbb.cs
+++ Arboles/Arbolbb.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,18 +10,101 @@ namespace Arboles
 {
     internal class Arbolbb
     {
-        Nodo raiz;
+        private Nodo raiz { get; set; }
 
         public Arbolbb()
         {
             raiz = null;
         }
+        public void Insertar(int valor)
+        {
+            raiz = InsertarRecursivo(raiz, valor);
 
+        }
 
+        private Nodo InsertarRecursivo(Nodo r, int valor)
+        {
+            if (r == null)
+            {
+                return new Nodo(valor);
+            }
+            if (valor < r.Dato)
+            {
+                r.Izquierda = InsertarRecursivo(r.Izquierda, valor);
+            }
+            else if (valor > r.Dato)
+            {
+                r.Derecha = InsertarRecursivo(r.Derecha, valor);
+            }
+            return r;
 
+        }
+        public bool Buscar(int valor)
+        {
+            return BuscarRecursivo(raiz, valor);
+            
+        }
+        private bool BuscarRecursivo(Nodo r, int valor)
+        {
+            if (r == null)
+            {
+                Console.WriteLine("Valor no encontrado");
+                return false;
+            }
 
+            if (valor == r.Dato)
+            {
+                Console.WriteLine("Valor encontrado: " + valor);
+                return true;
+            }
+            else if (valor < r.Dato)
+            {
+                return BuscarRecursivo(r.Izquierda, valor);
+            }
+            else
+            {
+                return BuscarRecursivo(r.Derecha, valor);
+            }
 
-
-
+        }
+        public void InOrden()
+        {
+            InOrdenRecursivo(raiz);
+        }
+        private void InOrdenRecursivo(Nodo r)
+        {
+            if (r != null)
+            {
+                InOrdenRecursivo(r.Izquierda);
+                Console.Write(r.Dato + " ");
+                InOrdenRecursivo(r.Derecha);
+            }
+        }
+        public void PreOrden()
+        {
+            PreOrdenRecursivo(raiz);
+        }
+        private void PreOrdenRecursivo(Nodo r)
+        {
+            if (r != null)
+            {
+                Console.Write(r.Dato + " ");
+                PreOrdenRecursivo(r.Izquierda);
+                PreOrdenRecursivo(r.Derecha);
+            }
+        }
+        public void PostOrden()
+        {
+            PostOrdenRecursivo(raiz);
+        }
+        private void PostOrdenRecursivo(Nodo r)
+        {
+            if (r != null)
+            {
+                PostOrdenRecursivo(r.Izquierda);
+                PostOrdenRecursivo(r.Derecha);
+                Console.Write(r.Dato + " ");
+            }
+        }
     }
 }
--- Arboles/Nodo.cs
+++ Arboles/Nodo.cs
@@ -8,15 +8,15 @@ namespace Arboles
 {
     internal class Nodo
     {
-        int dato;
-        Nodo izquierda;
-        Nodo derecha;
+        public int Dato { get; set; }
+        public Nodo Izquierda { get; set; }
+        public Nodo Derecha { get; set; }
 
         public Nodo(int dato)
         {
-            this.dato = dato;
-            izquierda = null;
-            derecha = null;
+            Dato = dato;
+            Izquierda = null;
+            Derecha = null;
         }
     }
 }
